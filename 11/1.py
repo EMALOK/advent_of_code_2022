806daef029745a558eab466a08c1940c102a4d50
EMALOK <emanuele.riccilucchi@gmail.com>
2022-12-11 14:39:14 +0100

    day 11

---
+++ 11/1.py
@@ -0,0 +1,102 @@
+import pprint
+
+file = open("data.txt","r")
+
+lines = file.readlines()
+
+monkeys = []
+monkey_index = 0
+
+monkey_businnes = []
+
+def print_items():
+    for curr in monkeys:
+        print(curr["items"])
+
+for line in lines:
+    line = line.strip()
+
+    if line == "":
+        monkey_index += 1
+        print("new monkey",monkey_index)
+    else:
+        
+        splits = line.split(" ")
+
+        if splits[0] == "Monkey":
+
+            monkeys.append(dict())
+            monkey_businnes.append(0)
+
+        elif splits[0] == "Starting":
+            
+            numbers = splits[2:]
+
+            for (i,num) in enumerate(numbers):
+                
+                if i != len(numbers) - 1:
+                    numbers[i] = num[:-1]
+
+            for (i,num) in enumerate(numbers):
+                numbers[i] = int(num)
+
+            monkeys[monkey_index]["items"] = numbers
+
+        elif splits[0] == "Operation:":
+
+            monkeys[monkey_index]["operation"] = line.split(":")[1].strip().split("=")[1].strip()
+
+        elif splits[0] == "Test:":
+
+            monkeys[monkey_index]["test"] = int(splits[3])
+
+        elif splits[0] == "If":
+
+            monkeys[monkey_index][splits[1][:-1]] = int(splits[5])
+
+pprint.pprint(monkeys)
+
+
+for round in range(1,21):
+
+    print("new round")
+
+    for (i,curr_monkey) in enumerate(monkeys):
+
+        print("new monkey")
+
+
+        for curr_item in curr_monkey["items"]:
+
+            monkey_businnes[i] += 1
+
+            old = curr_item
+
+            print("old",old)
+            print("operation",curr_monkey["operation"])
+            new = eval(curr_monkey["operation"])
+
+            new = new // 3
+
+            print("worry level",new)
+
+            print_items()
+            
+            if new % curr_monkey["test"] == 0:
+                #true
+                monkeys[curr_monkey["true"]]["items"].append(new)
+            else:
+                #false
+                monkeys[curr_monkey["false"]]["items"].append(new)
+        curr_monkey["items"].clear()
+                
+    print()
+    print_items()
+
+print(monkey_businnes)
+
+max1 = max(monkey_businnes)
+monkey_businnes.remove(max1)
+max2 = max(monkey_businnes)
+
+print(max1 * max2)
